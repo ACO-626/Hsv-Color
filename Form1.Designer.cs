fc416d0f0e6232350b85c9ee5e53b66ae669a43f
Alejandro Ocampo Cisneros <ocampoalejandro.oc@gmail.com>
2020-11-30 08:45:41 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Hsv_Color
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackHue = new System.Windows.Forms.TrackBar();
+            this.trackSaturation = new System.Windows.Forms.TrackBar();
+            this.trackValue = new System.Windows.Forms.TrackBar();
+            this.btnImportar = new System.Windows.Forms.Button();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackHue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackSaturation)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(79, 133);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(377, 270);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trackHue
+            // 
+            this.trackHue.Location = new System.Drawing.Point(79, 460);
+            this.trackHue.Name = "trackHue";
+            this.trackHue.Size = new System.Drawing.Size(266, 45);
+            this.trackHue.TabIndex = 3;
+            // 
+            // trackSaturation
+            // 
+            this.trackSaturation.Location = new System.Drawing.Point(79, 501);
+            this.trackSaturation.Name = "trackSaturation";
+            this.trackSaturation.Size = new System.Drawing.Size(266, 45);
+            this.trackSaturation.TabIndex = 4;
+            // 
+            // trackValue
+            // 
+            this.trackValue.Location = new System.Drawing.Point(79, 552);
+            this.trackValue.Name = "trackValue";
+            this.trackValue.Size = new System.Drawing.Size(266, 45);
+            this.trackValue.TabIndex = 5;
+            // 
+            // btnImportar
+            // 
+            this.btnImportar.Location = new System.Drawing.Point(37, 52);
+            this.btnImportar.Name = "btnImportar";
+            this.btnImportar.Size = new System.Drawing.Size(114, 49);
+            this.btnImportar.TabIndex = 6;
+            this.btnImportar.Text = "IMPORTAR";
+            this.btnImportar.UseVisualStyleBackColor = true;
+            this.btnImportar.Click += new System.EventHandler(this.btnImportar_Click);
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(174, 52);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(114, 49);
+            this.btnCargar.TabIndex = 7;
+            this.btnCargar.Text = "CARGAR";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(655, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(415, 245);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(655, 273);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(415, 253);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(655, 552);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(415, 233);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1223, 744);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.btnImportar);
+            this.Controls.Add(this.trackValue);
+            this.Controls.Add(this.trackSaturation);
+            this.Controls.Add(this.trackHue);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackHue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackSaturation)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackHue;
+        private System.Windows.Forms.TrackBar trackSaturation;
+        private System.Windows.Forms.TrackBar trackValue;
+        private System.Windows.Forms.Button btnImportar;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+namespace Hsv_Color
+{
+    public partial class Form1 : Form
+    {
+        Image<Hsv, byte> img;
+        Image<Gray, byte> HueImg;
+        Image<Gray, byte> SaturaImg;
+        Image<Gray, byte> ValueImg;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImportar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    img = new Image<Hsv, byte>(ofd.FileName);
+                    pictureBox1.Image = img.ToBitmap();
+                }
+            }
+
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            HueImg = img[0];
+            SaturaImg = img[1];
+            ValueImg = img[2];
+
+            pictureBox2.Image = HueImg.Bitmap;
+            pictureBox3.Image = SaturaImg.Bitmap;
+            pictureBox4.Image = ValueImg.Bitmap;
+        }
+    }
+}
